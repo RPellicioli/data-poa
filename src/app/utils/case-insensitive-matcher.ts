5b84f899d2a27215c40742d92cf9422ba388176a
Ricardo Peliciolli <Ricardo@goimage.com.br>
2020-06-24 10:39:53 -0300

    fix-ident

--- src/app/utils/case-insensitive-matcher.ts
+++ src/app/utils/case-insensitive-matcher.ts
@@ -3,16 +3,20 @@ import { UrlSegment, Route, UrlSegmentGroup, UrlMatchResult } from "@angular/rou
 export namespace CaseInsensitiveMatcher {
     export function matcher(segments: UrlSegment[], segmentGroup: UrlSegmentGroup, route: Route): UrlMatchResult {
         const url = (route as any).matcherPath;
+
         if (url) {
             const matchSegments = url.split('/');
+
             if (matchSegments.length > segments.length || (matchSegments.length !== segments.length && route.pathMatch === 'full')) {
                 return null;
             }
 
             const consumed: UrlSegment[] = [];
             const posParams: { [name: string]: UrlSegment } = {};
+
             for (let index = 0; index < segments.length; ++index) {
                 const matchSegment = matchSegments[index];
+                
                 if (matchSegment) {
                     if (matchSegment.startsWith(':')) {
                         posParams[matchSegment.slice(1)] = segments[index];
